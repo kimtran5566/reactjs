c61019fbb422fc4126945b38bb97241ab3a127bb
kimtran5566 <lktran5566@gmail.com>
2023-12-25 20:33:41 +0700

    update bai 2

--- my-react-ts/src/Bai-02.tsx
+++ my-react-ts/src/Bai-02.tsx
@@ -1,18 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 
+// Function component không sử dụng hook
 function Clock(props) {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {props.date.toLocaleTimeString()}.</h2>
-      </div>
-    );
-  }
-  
-  function tick() {
-    return(<Clock date={new Date()} />);
-  }
-  
-  setInterval(tick, 1000);
+  const getCurrentTime = () => {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+    return `${hours}:${minutes}:${seconds}`;
+  };
+
+  // Khởi tạo state để lưu giữ thời gian
+  const [currentTime, setCurrentTime] = React.useState(getCurrentTime());
+
+  // Sử dụng setInterval để cập nhật thời gian mỗi giây
+  React.useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentTime(getCurrentTime());
+    }, 1000);
+
+    // Xóa interval khi component unmount
+    return () => clearInterval(intervalId);
+  }, []); // Empty dependency array để đảm bảo useEffect chỉ chạy một lần sau khi mount
+
+  return (
+    <div>
+      <h2>Thời gian hiện tại:</h2>
+      <p>{currentTime}</p>
+    </div>
+  );
+}
 
 export default Clock;
