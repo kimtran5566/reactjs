2a8be15b7acf590c61dad05c1f23e6eb7fff5669
kimtran5566 <lktran5566@gmail.com>
2023-12-25 20:30:09 +0700

    update bai4

--- my-react-ts/src/Bai-04.tsx
+++ my-react-ts/src/Bai-04.tsx
@@ -1,12 +1,46 @@
 
+  import React, { Component } from 'react';
 
-
-
-const BrowserDetails = () => (
-    <h4>Browser's details: {navigator.userAgent}</h4>
-  );
-
+  class BrowserInfo extends Component {
+    constructor(props) {
+      super(props);
   
-
-
-  export default BrowserDetails;
+      // Khởi tạo state
+      this.state = {
+        browserInfo: '',
+      };
+    }
+  
+    componentDidMount() {
+      // Lấy thông tin user agent
+      const userAgent = navigator.userAgent;
+  
+      // Kiểm tra xem trình duyệt là gì
+      if (userAgent.indexOf('Chrome') !== -1) {
+        this.setState({ browserInfo: 'Bạn đang sử dụng trình duyệt Chrome' });
+      } else if (userAgent.indexOf('Firefox') !== -1) {
+        this.setState({ browserInfo: 'Bạn đang sử dụng trình duyệt Firefox' });
+      } else if (userAgent.indexOf('Safari') !== -1) {
+        this.setState({ browserInfo: 'Bạn đang sử dụng trình duyệt Safari' });
+      } else if (userAgent.indexOf('Edge') !== -1) {
+        this.setState({ browserInfo: 'Bạn đang sử dụng trình duyệt Edge' });
+      } else if (userAgent.indexOf('MSIE') !== -1 || userAgent.indexOf('Trident') !== -1) {
+        this.setState({ browserInfo: 'Bạn đang sử dụng trình duyệt Internet Explorer' });
+      } else {
+        this.setState({ browserInfo: 'Không thể xác định trình duyệt' });
+      }
+    }
+  
+    render() {
+      return (
+        <div>
+          <p>{this.state.browserInfo}</p>
+          {/* Hiển thị toàn bộ user agent để kiểm tra các thông tin khác */}
+          <h4>User Agent: {navigator.userAgent}</h4>
+        </div>
+      );
+    }
+  }
+  
+  export default BrowserInfo;
+  
